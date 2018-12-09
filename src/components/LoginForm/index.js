f5c6395f98de47c1ea5edfa7ff1b7075ba9736b2
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-09 20:47:39 -0300

    Added aasic Login scene and LoginForm Component

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null,
+            password: null
+        };
+    }
+
+    render() {
+        return (
+            <></>
+        );
+    }
+}
+
+export default LoginForm;
