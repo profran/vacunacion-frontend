91599fcfdd7cc85fbae5d96252c5892e71181144
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-10 10:20:28 -0300

    Added main LoginForm components and functions to dispatch loginUser

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loginUser } from '../../redux/actions/auth';
 
 class LoginForm extends Component {
     constructor(props) {
@@ -9,11 +11,31 @@ class LoginForm extends Component {
         };
     }
 
+    handle_change = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState(prevstate => {
+            const newState = {
+                ...prevstate
+            };
+            newState[name] = value;
+            return newState;
+        });
+    };
+
+    handle_login = (e, creds) => {
+        this.props.loginUser(creds);
+    }
+
     render() {
         return (
-            <></>
+            <div>
+                <input name="username" type="text" placeholder="Email" onChange={this.handle_change} />
+                <input name="password" type="password" placeholder="Password" onChange={this.handle_change} />
+                <button onClick={e => this.handle_login(e, this.state)}>Login</button>
+            </div>
         );
     }
 }
 
-export default LoginForm;
+export default connect(null, { loginUser })(LoginForm);
