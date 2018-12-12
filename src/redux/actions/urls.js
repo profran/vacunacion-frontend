d9050de6ec4bcd557b63a22ce85831c8525fa021
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-12 18:37:47 -0300

    Added real API urls

--- src/redux/actions/urls.js
+++ src/redux/actions/urls.js
@@ -1,9 +1,9 @@
-export const BASE_URL = '';
+export const BASE_URL = 'https://vacunacion-225123.appspot.com/_ah/api/vacunacionendpoints/v1';
 
-export const TOKEN_AUTH_ENDPOINT = '';
+export const TOKEN_AUTH_ENDPOINT = '/login/';
 export const TOKEN_VERIFICATION_ENDPOINT = '';
 
-export const SIGN_UP_USER_ENDPOINT = '';
+export const SIGN_UP_USER_ENDPOINT = '/signup/';
 export const SIGN_UP_MEDIC_ENDPOINT = '';
 
-export const CARNETS_LIST_ENDPOINT = '';
+export const CARNETS_LIST_ENDPOINT = '/listUsersWithCarnet/';
